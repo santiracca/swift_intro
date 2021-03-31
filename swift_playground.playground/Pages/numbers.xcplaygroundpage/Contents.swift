a8718c001f037cce773a628c178a855cfec692c6
santiracca <santiagoracca7@gmail.com>
2021-03-31 12:11:52 -0400

    Initial Commit

---
+++ swift_playground.playground/Pages/numbers.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var age = 15
+var price = 10.99
+
+var aPrice: Float = 10.99
+
+
+var length = 10
+var width = 5
+
+let area = length * width
+print(area)
+
+
+var health = 100
+var poisonDamage = 15
+
+health = health - poisonDamage
+print(health)
+
+
+health -= poisonDamage
---
+++ swift_playground.playground/Pages/strings.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+
+var firstName = "Santi"
+var age = 20
+
+var newMessage: String = "Hi, my name is \(firstName) and I am \(age) years old"
+
+print(newMessage)
